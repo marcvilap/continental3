396d1e8933f15b4ae335a86928ef58c10e4a06c2
Marc Vila <marc@mhou.es>
2024-01-10 09:41:05 +0100

    change photo

--- astro.config.ts
+++ astro.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
+	base: '/bc/dejandohuella/neumaticoseficientes',
 	integrations: [tailwind({ applyBaseStyles: false })],
 })
